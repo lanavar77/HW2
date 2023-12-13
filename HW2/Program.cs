dfc8bf53895aeaf5ef422126e6923a596ade9a5e
Your Name <you@example.com>
2023-12-13 04:13:20 +0400

    forname

---
+++ HW2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HW2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lana Vardosanidze");
+           
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine(Console.ReadLine());
+            Console.WriteLine("This is my new project");
+        }
+    }
+}
